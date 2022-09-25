af1784e3e4e95f347ade7944fe4c7e0f5109fc24
demhack5 <demhack5@proton.me>
2022-09-25 09:49:14 +0300

    fix main

--- cmd/personhood-proof/main.go
+++ cmd/personhood-proof/main.go
@@ -3,17 +3,28 @@ package main
 import (
 	//"context"
 	"context"
+	"database/sql"
+	"log"
 	"net/http"
 	"time"
 
-	personhood_proof "internal/service/personhood-proof"
+	personhood_proof "personhood-proof/internal/service/personhood-proof"
 
 	"github.com/go-chi/chi"
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	sqlDb, err := sql.Open("postgres", "postgres://alsoalgo:@127.0.0.1:5432/personhood-proof?sslmode=disable&binary_parameters=yes")
+	if err != nil {
+		log.Fatalf("err during sqlDb: %v", err)
+	}
+	defer sqlDb.Close()
+
+	db := sqlx.NewDb(sqlDb, "postgres")
 
 	publicRouter := chi.NewRouter()
 
@@ -25,6 +36,6 @@ func main() {
 
 	interval := time.Minute
 
-	personhoodProofService := personhood_proof.NewPersonhoodProofService()
+	personhoodProofService := personhood_proof.NewPersonhoodProofService(db)
 	defer personhoodProofService.Start(ctx, interval)
 }
